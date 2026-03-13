39e828d7c4217f9fca52e7728d8fb53f5d69547f
isakshi-tech <sakshiiii890@gmail.com>
2026-03-13 18:48:41 +0530

    Added assets and navbar logic

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import Order from "./pages/Order";
+import Placeorder from "./pages/Placeorder";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<Placeorder />} />
+        <Route path="/orders" element={<Order />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShopContext = () => {
+  return <div>ShopContext</div>;
+};
+
+export default ShopContext;
